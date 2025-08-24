0e61ccaa2d1b425ce51935db5119942e71cc5f51
Todor Parushev <tpparushev@gmail.com>
2025-08-24 09:40:17 +0300

    Add files via upload

---
+++ Azure/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+
+  features {
+
+  }
+
+  subscription_id = "sub_id"
+}
+
